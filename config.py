b82e6da24faf3e57e345553183f5f7082261f3ec
Pranav Choudhary <choudharypranav128@gmail.com>
2026-04-18 23:47:58 +0530

    Minor twiks

--- config.py
+++ config.py
@@ -1,28 +1,15 @@
-import os
-from dotenv import load_dotenv
-import json
-
-load_dotenv()
- 
-AI_API_KEY = os.getenv("AI_API_KEY")
- 
-APP_TITLE = "FinAIzer - AI Financial Advisor"
-import firebase_admin
-from firebase_admin import credentials, firestore, auth
-
-import os
+import streamlit as st
 import json
 import firebase_admin
 from firebase_admin import credentials, firestore
 
+# ---------------- AI KEY ---------------- #
+AI_API_KEY = st.secrets["AI_API_KEY"]
+
+# ---------------- FIREBASE ---------------- #
 if not firebase_admin._apps:
-
-    if os.path.exists("firebase_config.json"):
-        cred = credentials.Certificate("firebase_config.json")
-    else:
-        cred_dict = json.loads(os.environ["FIREBASE_KEY"])
-        cred = credentials.Certificate(cred_dict)
-
+    cred_dict = json.loads(st.secrets["FIREBASE_CREDENTIAL"])
+    cred = credentials.Certificate(cred_dict)
     firebase_admin.initialize_app(cred)
 
 db = firestore.client()
